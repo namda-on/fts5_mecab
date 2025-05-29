e7a2cebd7878b166d96d57876475b071df4380e9
namda-on <namda1571@gmail.com>
2025-05-29 18:41:27 +0900

    fts_mecab build cmake로 작성

--- build.sh
+++ build.sh
@@ -8,22 +8,13 @@ DRY_RUN=0
 NAME=fts5_mecab
 SRC=$NAME.c
 
-# 플랫폼 별 확장자
-UNAME="$(uname)"
-if [[ "$UNAME" == "Darwin" ]]; then
-    OUT="lib${NAME}.dylib"
-else
-    OUT="lib${NAME}.so"
-fi
-
-
 mkdir -p $PREFIX/include $PREFIX/lib
 cp sqlite/sqlite3*.h $PREFIX/include/
 
 # Mecab 빌드
 (
   cd mecab/mecab
-  ./configure --prefix=$PREFIX --with-charset=UTF8
+  ./configure --prefix=$PREFIX --with-charset=UTF8 --enable-static --disable-shared
   make && make install
 )
 (
@@ -32,44 +23,4 @@ cp sqlite/sqlite3*.h $PREFIX/include/
   make && make install
 )
 
-
-usage_exit() {
-    echo "Usage: $0 [-h|--help] [--prefix=PREFIX] [--clear-cflags] [-DSYMBOL] [--dry-run]"
-    echo "  \$PREFIX=$PREFIX"
-    echo "  \$CFLAGS=\"$CFLAGS\""
-    echo "  \$DRY_RUN=$DRY_RUN"
-    echo "Sample"
-    echo "  $0 --prefix=$PREFIX --clear-cflags -DDEBUG -DSTOP789 --dry-run"
-    exit 0
-}
-
-echo_and_do() {
-    echo "$1"
-    if [[ "$DRY_RUN" -eq 0 ]]; then
-        eval "$1"
-    else
-        echo "!! DRY RUN !!"
-    fi
-}
-
-for x in "$@"; do
-    case "$x" in
-        -h|--help) usage_exit ;;
-        --dry-run) DRY_RUN=1 ;;
-        --prefix=*) PREFIX="${x#*=}" ;;
-        --clear-cflags) CFLAGS="" ;;
-        -D*) CFLAGS="$CFLAGS -D${x:2}" ;;
-        *) echo "Unknown option: $x"; usage_exit ;;
-    esac
-done
-
-
-[ ! -d $PREFIX/lib ] && mkdir -p $PREFIX/lib
-
-echo_and_do "$CC -g -fPIC -shared $SRC -o $OUT -I $PREFIX/include -L $PREFIX/lib -lmecab $CFLAGS"
-if [ $? == 0 ]; then
-  echo ""
-  echo "✅ Compilation succeeded."
-else
-    echo "❌ Compilation failed."
-fi
+echo "MeCab and IPADIC built into $PREFIX"
